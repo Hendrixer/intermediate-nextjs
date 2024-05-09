3d2a199281d6452ca8eef55d6b0e014a62065488
Scott Moss <willscottmoss@gmail.com>
2024-05-09 15:12:07 -0500

    start

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { getAttendeesCountForDashboard } from '@/utils/attendees'
 import { getCurrentUser } from '@/utils/users'
-
 const Home = async () => {
   const user = await getCurrentUser()
   const count = await getAttendeesCountForDashboard(user.id)
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,34 @@
 'use client'
+import { createNewEvent } from '@/actions/events'
 import { Input } from '@nextui-org/react'
 import { Button, Tooltip } from '@nextui-org/react'
 import { CirclePlus } from 'lucide-react'
+import { useTransition } from 'react'
 
 const Nav = () => {
+  const [isPending, startTransition] = useTransition()
+
+  const handleClick = () => {
+    startTransition(() => {
+      createNewEvent()
+    })
+  }
+
   return (
     <nav className="h-[65px] border-b border-default-50 flex items-center px-6 gap-4">
+      <div>
+        <Tooltip content="New Event">
+          <Button
+            isIconOnly
+            variant="ghost"
+            size="sm"
+            isLoading={isPending}
+            onClick={handleClick}
+          >
+            <CirclePlus size={16} />
+          </Button>
+        </Tooltip>
+      </div>
       <div className="w-1/2">
         <Input size="sm" variant="faded" placeholder="search" />
       </div>
