f78561dca80a9ad96a4418e9f076eb707f159ea1
Frank Stallone <fstallone@strongtie.com>
2024-11-19 13:32:27 -0500

    bug: 🐛 Fixing issue with Drizzle needing to be on latest versions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './db/schema.ts',
   out: './migrations',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
